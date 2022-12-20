766b4a882a9472bac712fe7ebf8e1fa39a321043
karimchamas <121017476+karimchamas@users.noreply.github.com>
2022-12-20 12:10:13 +0100

    main branche

--- src/test/java/projetJPA/test/mainTest.java
+++ src/test/java/projetJPA/test/mainTest.java
@@ -10,7 +10,7 @@ import projetJPA.util.JpaContext;
 public class mainTest {
 
 	public static void main(String[] args) {
-		
+		// branch main
 		JpaContext.getEntityManagerFactory();
 		DaoEmploye daoEmployee = JpaContext.getDaoEmploye();
 		DaoDepartement daoDepartment = JpaContext.getDaoDepartment();
